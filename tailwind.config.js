af7a2c49e352c9d438444234b6f5ed9791dcf032
harikrishnan2193 <harikrishnank2193@gmail.com>
2025-06-01 19:45:50 +0530

    update container utility on tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+      },
+    },
     extend: {
       fontFamily: {
         raleway: ['Raleway', 'serif'],
